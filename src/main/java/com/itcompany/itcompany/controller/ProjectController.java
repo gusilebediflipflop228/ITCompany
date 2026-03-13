5f46ee5f3f1dd260b44c26cdc80f6ebedfbb1643
gusilebediflipflop228 <yurikikadet@gmail.com>
2026-03-13 16:19:26 +0600

    feat: complete lab1 tasks

--- src/main/java/com/itcompany/itcompany/controller/ProjectController.java
+++ src/main/java/com/itcompany/itcompany/controller/ProjectController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+// чисто для коммитаgit branch
 @RestController
 @RequestMapping("/api/projects")
 @RequiredArgsConstructor
